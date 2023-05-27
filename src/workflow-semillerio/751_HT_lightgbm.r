d751849319e55cdc06e3d6ecb64aeab05e7d56bc
monicaorduz <monica.orduz@uptc.edu.co>
2023-05-26 23:29:08 -0500

    Ajuste parámetros semillerio

--- src/workflow-semillerio/751_HT_lightgbm.r
+++ src/workflow-semillerio/751_HT_lightgbm.r
@@ -79,16 +79,16 @@ PARAM$bo_lgb <- makeParamSet(
          makeNumericParam("learning_rate",    lower=    0.02, upper=     0.3),
          makeNumericParam("feature_fraction", lower=    0.01, upper=     1.0),
          makeIntegerParam("num_leaves",       lower=    8L,   upper=  1024L),
-         makeIntegerParam("min_data_in_leaf", lower=  100L,   upper= 50000L)
+         makeIntegerParam("min_data_in_leaf", lower=  100L,   upper= 50000L),
          ## Agregados Lamdas y min GTS
-         makeNumericParam("lambda_l1", lower=  0.0,   upper= 1000.0)
-         makeNumericParam("lambda_l2", lower=  0.0,   upper= 1000.0)
+         makeNumericParam("lambda_l1", lower=  0.0,   upper= 1000.0),
+         makeNumericParam("lambda_l2", lower=  0.0,   upper= 1000.0),
          makeNumericParam("min_gain_to_split", lower=  0.0,   upper= 20.0)
         )
 
 
 #si usted es ambicioso, y tiene paciencia, podria subir este valor a 100
-PARAM$bo_iteraciones  <- 50  #iteraciones de la Optimizacion Bayesiana
+PARAM$bo_iteraciones  <- 30  #iteraciones de la Optimizacion Bayesiana
 
 PARAM$home  <- "~/buckets/b1/"
 
